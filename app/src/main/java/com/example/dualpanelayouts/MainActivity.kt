055636a80757bebd0183f4da49fb87d34f00b3e5
Fahim Ahmed <p2595607@my365.dmu.ac.uk>
2023-03-11 00:57:57 +0000

    StarSignListener works on tablet screen

--- app/src/main/java/com/example/dualpanelayouts/MainActivity.kt
+++ app/src/main/java/com/example/dualpanelayouts/MainActivity.kt
@@ -1,19 +1,37 @@
 package com.example.dualpanelayouts
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+
+const val STAR_SIGN_ID = "STAR_SIGN_ID"
 
 interface StarSignListener {
     fun onSelected(id: Int)
 }
 
 class MainActivity : AppCompatActivity(), StarSignListener {
-    override fun onSelected(id: Int) {
 
-    }
+    var isDualPane: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        isDualPane = findViewById<View>(R.id.star_sign_detail) != null
+    }
+
+    override fun onSelected(id: Int) {
+
+        if (isDualPane) {
+            val detailFragment = supportFragmentManager.findFragmentById(R.id.star_sign_detail) as DetailFragment
+
+            detailFragment.setStarSignData(id)
+        } else {
+            val detailIntent = Intent(this, DetailFragment::class.java).apply {
+                putExtra(STAR_SIGN_ID, id)
+            }
+            startActivity(detailIntent)
+        }
     }
 }
